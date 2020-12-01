ce5905dee11de3b6c89f24bf52cae683d6348165
이은덕 <pleed0215@ieundeog-ui-MacBookPro.local>
2020-12-02 00:34:08 +0900

    code challenge need

--- README.md
+++ README.md
@@ -6,3 +6,32 @@
 
 - Webdriver manager for python 설치.
 - pipenv install webdriver_manager
+
+## 2. WebElement
+
+## 3. Error handling
+
+    - try except를 for loop에 사용시 error가 발생해도 loop를 돌리고 싶다면??
+        - except에 continue를 써주면 된다.
+
+## 4. enumerate
+
+    - iterating에서 아이템과 index를 같이 얻어 올 수 있다.
+
+## 5. Waiting for elements
+
+    - javascript 실행 되는 시간 때문에 더러 원하는 결과를 얻지 못할 때가 있다.
+    ```python
+        shitty_element = WebDriverWait(browser, 5).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "g-blk")))
+    ```
+    - By - locator, selenium.webdriver.common.by 에서 import
+    - expected_conditions -> selenium.webdriver.support에서 import
+    - WebDriverWait -> selenium.webddriver.support.ui 에서 import
+    - 코드는 최대 5초까지 기다리면서... classname이 g-blk가 포함되는 element가 js에 의하여 located될 때까지 기다리라는 의미..
+
+## 6. javascript를 이용하여 shitty_element를 없애기
+
+    - 참고로 python에서는 multiline script는 """ """
+    - browser.execute_script(script, *args)
+        - args는 arguments[0] ... 으로 들어간다.
+    - selenium에서 찾은 element를 script함수를 통해 보내면, html element가 된다.
--- main.py
+++ main.py
@@ -1,19 +1,88 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.common import exceptions
 #from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
+# constant
+KEYWORD = "buy domain"
 
-browser.get('https://google.com')
 
 
-search_bar = browser.find_element_by_class_name('gLFyf')
-search_bar.send_keys('hello', Keys.RETURN)
 
-search_results = browser.find_elements_by_class_name('g')
+class GoogleKeywordScreenShooter():
+    def __init__(self, keyword, dir="screenshots"):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        self.keyword = keyword
+        self.dir = dir
 
-for result in search_results:
-    print(dir(result))
+    def start(self):
+        self.browser.get('https://google.com')
+        try:
+            search_bar = self.browser.find_element_by_class_name('gLFyf')
+            search_bar.send_keys(self.keyword, Keys.RETURN)
 
-browser.quit()
+            shitty_element = WebDriverWait(self.browser, 5).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "g-blk")))
+            self.browser.execute_script("""
+                const shitty = arguments[0];
+                shitty.parentElement.removeChild(shitty);
+            """, shitty_element)
+            search_results = self.browser.find_elements_by_class_name('g')
+
+            print(f"Count of Results: {len(search_results)}")
+            for i,result in enumerate(search_results):
+                if 'kno-kp' not in result.get_attribute('class'):
+                    result.screenshot(f"{self.dir}/{self.keyword}-{i}.png")
+        finally:
+            self.browser.quit() 
+
+
+"""
+def get_titles(keyword):
+    try:
+        search_bar = browser.find_element_by_class_name('gLFyf')
+        search_bar.send_keys(keyword, Keys.RETURN)
+
+        search_results = browser.find_elements_by_class_name('g')
+        
+
+        print(f"Count of Results: {len(search_results)}")
+        for result, i in enumerate(search_results):
+            try:
+                title = result.find_element_by_tag_name('h3');
+                if title is not None:
+                    title.screentshot()
+            except exceptions.NoSuchElementException:
+                print("An error is occured.")
+                continue
+    finally:
+        browser.quit() 
+
+
+def get_screentshots(keyword):
+    try:
+        search_bar = browser.find_element_by_class_name('gLFyf')
+        search_bar.send_keys(keyword, Keys.RETURN)
+
+        shitty_element = WebDriverWait(browser, 5).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "g-blk")))
+        browser.execute_script('''
+            const shitty = arguments[0];
+            shitty.parentElement.removeChild(shitty);
+        , shitty_element)
+        search_results = browser.find_elements_by_class_name('g')
+
+        print(f"Count of Results: {len(search_results)}")
+        for i,result in enumerate(search_results):
+            if 'kno-kp' not in result.get_attribute('class'):
+                result.screenshot(f"{keyword}-{i}.png")
+    finally:
+        browser.quit() 
+"""
+
+#get_titles(KEYWORD)
+#get_screentshots(KEYWORD)
+shotter = GoogleKeywordScreenShooter(KEYWORD)
+shotter.start()
